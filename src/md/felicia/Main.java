a7360948cc39624cad83ff79215aa6482b903dd4
AlexLX2 <kolosoff.alex@gmail.com>
2019-09-17 13:04:28 +0300

    ver 0.2 added downloading file
    Signed-off-by: AlexLX2 <kolosoff.alex@gmail.com>

--- src/md/felicia/Main.java
+++ src/md/felicia/Main.java
@@ -6,15 +6,20 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.BufferedInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.*;
 import java.net.URL;
 import java.net.URLDecoder;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
 
 public class Main {
 
@@ -22,59 +27,89 @@ public class Main {
 
     public static void main(String[] args) {
         String catalog = "";
-        String decoded = "";
+        String decoded;
         Elements cataloglinks = null;
         Document doc;
-        try{
+        try {
             doc = Jsoup.connect("http://amed.md/ro/catalogul-national").get();
 
             cataloglinks = doc.body().getElementsByClass("field-item even");
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
-        for(Element link : cataloglinks) {
+        assert cataloglinks != null;
+        for (Element link : cataloglinks) {
             catalog = link.getElementsByTag("a").attr("href");
         }
 
-        try {
-            decoded = URLDecoder.decode(catalog,"UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+        decoded = URLDecoder.decode(catalog, StandardCharsets.UTF_8);
 
         decoded = decoded.substring(decoded.lastIndexOf(" "));
-        decoded = decoded.substring(0,decoded.lastIndexOf("."));
+        decoded = decoded.substring(0, decoded.lastIndexOf("."));
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-        Date date = new Date();
-        try {
-            date = sdf.parse(decoded);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(catalog);
-        System.out.println(decoded);
-        System.out.println(date);
-
-
-        downloadCatalog(catalog,decoded);
-
-
+        String fileStringPath = FILE_NAME + decoded + ".xls";
+        File checkFile = new File(fileStringPath);
+        if (!checkFile.exists()) {
+            downloadCatalog(catalog, fileStringPath);
+            sendMail(fileStringPath);
+        } else
+            System.out.println("Catalog already downloaded");
 
     }
 
-    public  static  void downloadCatalog(String url, String file){
+    private static void downloadCatalog(String url, String fileStringPath) {
+
+
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream  = new FileOutputStream(FILE_NAME + file + ".xls")) {
-            byte dataBuffer[] = new byte[1024];
+             FileOutputStream fileOutputStream = new FileOutputStream(fileStringPath)) {
+            byte[] dataBuffer = new byte[1024];
             int bytesRead;
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
             }
         } catch (IOException e) {
-          e.printStackTrace();
+            e.printStackTrace();
         }
+
+
+    }
+
+    private static void sendMail(String file) {
+        String to = "ak@felicia.md";         // sender email
+        String from = "ak@felicia.md";       // receiver email
+        String host = "10.50.0.21";            // mail server host
+
+        Properties properties = System.getProperties();
+        properties.setProperty("mail.smtp.host", host);
+
+        Session session = Session.getDefaultInstance(properties); // default session
+
+        try {
+            MimeMessage message = new MimeMessage(session); // email message
+
+            message.setFrom(new InternetAddress(from)); // setting header fields
+
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+            message.setSubject("New catalog has arrived"); // subject line
+
+            // actual mail body
+            message.setText("Вышел новый каталог");
+
+            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+            mimeBodyPart.attachFile(file);
+
+            MimeMultipart multipart = new MimeMultipart("related");
+            multipart.addBodyPart(mimeBodyPart);
+
+            message.setContent(multipart);
+            // Send message
+            Transport.send(message);
+            System.out.println("Email Sent successfully....");
+        } catch (MessagingException | IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
